6a900016376062f600ee54fdc95574c9d6ba2a9a
Mrunmoy Samal <mrunmoy@gmail.com>
2018-05-24 22:57:31 +1000

    wip

--- main.c
+++ main.c
@@ -10,22 +10,17 @@ typedef enum path_t {
     PATH_Right
 }path_t;
 
-typedef struct node_t {
-    int index;
-    int left;//struct node_t *pLeft;
-    int right; //struct node_t *pRight;
-}node_t;
-
-static bool has_valid_path_recursive(int ** const pArray, const int N, const int src_i, const int dst_i)
+static bool has_valid_path_recursive(int ** const pArray, int ** const pVisited, const int N, const int src_i, const int dst_i)
 {
     for (int i = 0; i < N; i++)
     {
-        if (pArray[src_i][i]) {// if path exists ?
+        if (pArray[src_i][i] && (!pVisited[src_i][i])) {// if path exists ?
+            pVisited[src_i][i] = 1;
             if (i == dst_i) {
                 return true;
             }
             else {
-                return has_valid_path_recursive(pArray, N, i, dst_i);
+                return has_valid_path_recursive(pArray, pVisited, N, i, dst_i);
             }
         }
     }
@@ -39,8 +34,8 @@ static bool has_valid_path_recursive(int ** const pArray, const int N, const int
  **/
 int main()
 {
-    int N = 4; // the total number of nodes in the level, including the gateways
-    int L = 4; // the number of links
+    int N = 12; // the total number of nodes in the level, including the gateways
+    int L = 23; // the number of links
     int E = 1; // the number of exit gateways
     //scanf("%d%d%d", &N, &L, &E);
     fprintf(stderr,"%d%d%d\n", N, L, E);
@@ -50,8 +45,13 @@ int main()
     for (int i = 0; i < N; i++) {
         board[i] = (int *)calloc(N, sizeof(int));
     }
+    int **visited = (int **)calloc(N, sizeof(int *));
+    for (int i = 0; i < N; i++) {
+        visited[i] = (int *)calloc(N, sizeof(int));
+    }
 
-    int arr[4][2] = { {1,3}, {2,3}, {0,1}, {0,2} };
+    int arr[23][2] = { {11,6}, {0,9}, {1,2}, {0,1}, {10,1}, {11,5}, {2,3}, {4,5}, {8,9}, {6,7}, {7,8}, {0,6}, {3,4}, {0,2}, {11,7}, {0,8}, {0,4}, {9,10}, {0,5}, {0,7}, {0,3}, {0,10}, {5,6}  };
+
     for (int i = 0; i < L; i++) {
         int N1 = arr[i][0]; // N1 and N2 defines a link between these nodes
         int N2 = arr[i][1];
@@ -63,14 +63,14 @@ int main()
     fprintf(stderr,"Exit nodes at: ");
     int *exit_nodes = (int *)calloc(E, sizeof(int));
     for (int i = 0; i < E; i++) {
-        int EI = 3; // the index of a gateway node
+        int EI = 0; // the index of a gateway node
         // scanf("%d", &EI);
         fprintf(stderr,"%d ", EI);
         exit_nodes[i] = EI;
     }
     fprintf(stderr,"\n");
 
-    int SI_List[2] = {0, 2};
+    int SI_List[2] = {11, 2};
     int iter = 0;
     do // game loop
     {
@@ -81,13 +81,16 @@ int main()
         // To debug: fprintf(stderr, "Debug messages...\n");
         for (int ea = 0; ea < E; ea++)
         {
+            for (int i = 0; i < N; i++) {
+                memset(visited[i], 0, N * sizeof(int));
+            }
             fprintf(stderr, "Looking at Exit node: %d\n", exit_nodes[ea]);
             int index_of_exit = exit_nodes[ea];
             bool done = false;
             for (int i = 0; i < N; i++) {
                 if (board[index_of_exit][i]) {
                     // quick look
-                    if ( (i == SI) || has_valid_path_recursive(board, N, i, SI) ) { //sever this link
+                    if ( (i == SI) || has_valid_path_recursive(board, visited, N, i, SI) ) { //sever this link
                         board[index_of_exit][i] = 0;
                         board[i][index_of_exit] = 0;
                         fprintf(stderr, "Path from %d to %d via %d severed!\n", index_of_exit, SI, i);
@@ -112,6 +115,7 @@ int main()
     } while(iter < 2);
 
     free(board);
+    free(visited);
     free(exit_nodes);
 
     return 0;
